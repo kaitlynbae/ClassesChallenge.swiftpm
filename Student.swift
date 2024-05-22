72cf0a6ecef833718480c7a8b74984ddd89af413
Kaitlyn Bae <kaitlynbae07@gmail.com>
2024-05-22 11:30:48 -0700

    Initial Commit

---
+++ Student.swift
@@ -0,0 +1,43 @@
+//
+//  Student.swift
+//  ClassesChallenge
+//
+//  Created by Robert Englund
+//  Copyright © 2022 MobileMakersEdu. All rights reserved.
+//
+
+
+// MARK: MVP - Part I
+class Student{
+    var firstName = "Kaitlyn"
+    var lastName = "Bae"
+    
+    
+    
+    
+    
+    
+    // MARK: Stretch #1 - Part I
+    var idNumber: String = ""
+    var favoriteColor: String = ""
+    init(){
+        idNumber = "225024"
+        favoriteColor = "Green"
+    }
+    
+}
+
+    
+
+
+    // MARK: Stretch #2 - Part I
+    
+
+
+
+
+    // MARK: - Stretch #3 - Part I
+    
+
+
+
